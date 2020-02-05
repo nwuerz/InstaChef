7ffe67023819f865ba97a2e18810a605875820ab
nick wuerz <nawuerz@gmail.com>
2020-02-04 20:49:14 -0600

    created javascript file and created vars and ajax call

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// we have alot to do!! :(.....
+$(document).ready(function () { 
+
+var proteinEl = $("proteinEl");
+var veggiesEl = $("veggieEl");
+var spicesEl = $("spicesEl");
+var carbsEl = $("carbsEl");
+var findEl = $("findEl");
+
+// when user clicks findEl button, display the first three options
+findEl.on("click", function() {
+    var recipeURL = "http://www.recipepuppy.com/api/?i=" + proteinEl.val() + veggiesEl.val() + spicesEl.val() + carbsEl.val();
+    $.ajax({
+        url: recipeURL,
+        method: 'GET',
+      }).then(function(response) {
+        console.log(response);
+      });
+// when user clicks next, display the next 3 options
+
+// when the user selects an option, display
+});
+
+});
