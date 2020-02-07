37b3c8258caef2aa38edf1bea732dca129d41be7
nick wuerz <nawuerz@gmail.com>
2020-02-06 19:48:06 -0600

    created css to display and hide divs as needed

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $(document).ready(function () { 
 
-var proteinEl = $("proteinEl");
-var veggiesEl = $("veggieEl");
-var spicesEl = $("spicesEl");
-var carbsEl = $("carbsEl");
-var findEl = $("findEl");
+var proteinEl = $("#proteinEl");
+var veggiesEl = $("#veggieEl");
+var spicesEl = $("#spicesEl");
+var carbsEl = $("#carbsEl");
+var findEl = $("#findEl");
+var startDiv = $("#startDiv");
+var optionsDiv = $("#optionDiv");
 
-// when user clicks findEl button, display the first three options
+// when user clicks findEl button... //change startDiv attribute to hide start page and display optionsDiv
 findEl.on("click", function() {
     var recipeURL = "http://www.recipepuppy.com/api/?i=" + proteinEl.val() + veggiesEl.val() + spicesEl.val() + carbsEl.val();
     $.ajax({
@@ -14,8 +16,13 @@ findEl.on("click", function() {
         method: 'GET',
       }).then(function(response) {
         console.log(response);
+        //change startDiv attribute to hide start page and display optionsDiv
+        startDiv.attr("class", "hideDiv");
+        optionsDiv.attr("class", "displayDiv");
+        //append results from returned object to corresponding elements
+        
       });
-// when user clicks next, display the next 3 options
+//clear 
 
 // when the user selects an option, display
 });
