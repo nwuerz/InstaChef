ad6486eeeeb3f6f03392aaebf1a335ff9de3139e
nick wuerz <nawuerz@gmail.com>
2020-02-08 13:37:03 -0600

    added ability to grab ingredients that user selects to be used in ajax call to api

--- script.js
+++ script.js
@@ -1,63 +1,60 @@
 $(document).ready(function () { 
 
-var proteinEl = $("#proteinEl");
-var veggiesEl = $("#veggieEl");
-var spicesEl = $("#spicesEl");
-var carbsEl = $("#carbsEl");
-var findEl = $("#findEl");
-var startDiv = $("#startDiv");
-var optionsDiv = $("#optionDiv");
-var apiKey = "&apiKey=103be80050034030a9270b7a9de5630f";
-//protein options
-var chickenEl = $("#chicken");
-var porkEl = $("pork");
-var groundBeefEl = $("groundBeef");
-var fishEl = $("fish");
-var tofuEl = $("tofu");
-//veggie options
-var cornEl = $("corn");
-var spinachEl = $("spinach");
-var greenbeanEl = $("greenBeans");
-var potatoeEl = $("potatoes");
-var squashEl = $("squash");
-//spice options
-var saltEl = $("salt");
-var pepperEl = $("pepper");
-var crushedRedPepperEl = $("crushedRedPepper");
-var cayenneEl = $("cayenne");
-var ThymeEl = $("thyme");
-//carbs options
-var whiteRiceEl = $("whiteRice");
-var brownRiceEl = $("brownRice");
-var pastaEl = $("pasta");
-var tortillaEl = $("tortillas");
-var breadEl = $("bread");
+  //protein options
+  var $chicken = $("#chicken");
+  var $pork = $("#pork");
+  var $groundBeef = $("#groundBeef");
+  var $fish = $("#fish");
+  var $tofy = $("#tofu");
+  //veggie options
+  var $corn = $("#corn");
+  var $spinach = $("#spinach");
+  var $greenBeans = $("#greenBeans");
+  var $potatoes = $("#potatoes");
+  var $squash = $("#squash");
+  //spice options
+  var $salt = $("#salt");
+  var $pepper = $("#pepper");
+  var $crushedRedPepper = $("#crushedRedPepper");
+  var $cayenne = $("#cayenne");
+  var $thyme = $("#thyme");
+  //carbs options
+  var $whiteRice = $("#whiteRice");
+  var $brownRice = $("#brownRice");
+  var $pasta = $("#pasta");
+  var $tortillas = $("#tortillas");
+  var $bread = $("#bread");
 
+  var $findRecipe = $("#findRecipe");
+  var apiKey = "&apiKey=103be80050034030a9270b7a9de5630f";
+  var ingredientsArr = [];
 
 
 // when user clicks findEl button... //change startDiv attribute to hide start page and display optionsDiv
-findEl.on("click", function() {                             
-    var recipeURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + proteinEl.val() + veggiesEl.val() + spicesEl.val() + carbsEl.val(); + apiKey;
+$findRecipe.on("click", function() {    
+  // console.log($(".form-check-input")); 
+  $(".form-check-input").each(function(index, checkbox){
+    // console.log(checkbox);
+    var isChecked = $(checkbox).prop("checked");
+    var ingredientVal = $(checkbox).val();
+    console.log(ingredientVal + " " + isChecked);
+    if(isChecked === true) {
+      ingredientsArr.push(ingredientVal);
+    }
+    
+  });                        
+  console.log(ingredientsArr);
+  var ingredientsJoined = ingredientsArr.join("+");
+    var recipeURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredientsJoined + apiKey;
     $.ajax({
         url: recipeURL,
         method: 'GET',
       }).then(function(response) {
         console.log(response);
-        //change startDiv attribute to hide start page and display optionsDiv
-        startDiv.attr("class", "hideDiv");
-        optionsDiv.attr("class", "displayDiv");
-        //append results from returned object to corresponding elements
-        for (let i = 0; i < array.length; i++) {
-          //create elements
-          var optionImg = $("<img>");
-          optionImg.attr("src", "response[i].image");
-          optionsDiv.append(optionImg);
-          var optionBtn = $("<button>");
-          optionBtn.text("option " + response[i]+1);
-        }
-
-
       });
+
+
+
 //clear 
 
 // when the user selects an option, display
